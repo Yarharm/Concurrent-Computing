ea975df49343db8ea165ff64460f2153fabdbcad
yarharm <yarharm@gmail.com>
2020-09-26 18:38:39 -0400

    Implement dynamic spawn for PI calculation

---
+++ Parallelization/PI_Calculation/README.md
@@ -0,0 +1,5 @@
+# PI Calculator
+
+### Issues
+
+- Dynamic spawn hangs for more than one processor.
--- Parallelization/PI_Calculation/sequential/seq_pi.cpp
+++ Parallelization/PI_Calculation/sequential/seq_pi.cpp
@@ -6,38 +6,35 @@
 #include <sys/time.h>
 #include <math.h>
 
-double calcPI(int darts);
+int countDartsInCircle(int darts);
 void srand(unsigned seed);
-#define DARTS 50000 /* number of throws at dartboard */
-#define ROUNDS 100  /* number of times "darts" is iterated */
+#define TOTAL_DARTS 10000000 /* number of throws at dartboard */
 
 struct timeval stop, start;
 int main()
 {
-  double pi, avepi;
+  double pi;
+  int totalDartsInCircle;
   srand(time(0));
   gettimeofday(&start, NULL);
 
-  for (int i = 0; i < ROUNDS; i++)
-  {
-    pi = calcPI(DARTS);
+  totalDartsInCircle = calcPI(TOTAL_DARTS);
 
-    /* Compute average for this iteration and all iterations */
-    avepi = ((avepi * i) + pi) / (i + 1);
-    printf("After %8d throws, average value of pi = %10.8f\n", (DARTS * (i + 1)), avepi);
-  }
-  
+  /* Compute average for this iteration and all iterations */
+  pi = 4 * (double)totalDartsInCircle / TOTAL_DARTS;
+  printf("pi = %10.8f\n", pi);
+
   gettimeofday(&stop, NULL);
   printf("\nReal value of PI: 3.1415926535897 \n");
   long seconds = stop.tv_sec - start.tv_sec;
   long micro_seconds = stop.tv_usec - start.tv_usec;
   long total_micro_seconds = (seconds * 1000000) + abs(micro_seconds);
-  
-  printf("\nExecution time is: %10.8f miliseconds\n", (double) total_micro_seconds / 1000);
+
+  printf("\nExecution time is: %10.8f miliseconds\n", (double)total_micro_seconds / 1000);
   return 0;
 }
 
-double calcPI(int darts)
+int countDartsInCircle(int darts)
 {
   double x_coord, y_coord, pi, r, dist;
   int score, n;
@@ -57,6 +54,5 @@ double calcPI(int darts)
       score++;
     }
   }
-  pi = 4.0 * (double)score / (double)darts;
-  return pi;
+  return score;
 }
